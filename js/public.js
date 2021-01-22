c8441810334539a02fd2faf954b4dcd6a53feca4
SaSaChu <teresa7397@gmail.com>
2021-01-22 22:03:18 +0800

    W+M index ok

--- js/public.js
+++ js/public.js
@@ -4,4 +4,26 @@ $(function() {
 
   $(".pro01_img, .pro02_img, .pro02_img_five, .pro03_img").imgLiquid ();
 
+
+  $('#r_menu_boxs').each (function () {
+    var $that = $(this);
+    $that.find ('.m-menu-t').click (function () {
+      $(this).toggleClass ('s');
+    });
+  });
+  
+  $('.icon-menu').click(function() {
+    // $('.main').addClass('nav_hover');
+    $('#r_menu_boxs').addClass('nav_open');
+    // $('#menu_back ').addClass('b_open');
+    // scrollLock();
+  });
+
+  $('.icon-x').click(function() {
+    // $('.main').removeClass('nav_hover');
+    $('#r_menu_boxs').removeClass('nav_open');
+    // $('#menu_back').removeClass('b_open');
+    // scrollUnlock();
+  });
+
 });
