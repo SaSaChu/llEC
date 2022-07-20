898d42e7e4f167cde4e30c477d8bb6f8d48e9718
SaSaChu <teresa7397@gmail.com>
2022-07-20 13:20:25 +0800

    修改商城顏色

--- js/public.js
+++ js/public.js
@@ -146,49 +146,6 @@ $(function() {
 
   })
 
-
-
-
-  // // 購物車加減數量
-  // var num_jia = document.getElementById("num-jia");
-  //   var num_jian = document.getElementById("num-jian");
-  //   var input_num = document.getElementById("input-num");
-
-  //   num_jia.onclick = function() {
-
-  //     input_num.value = parseInt(input_num.value) + 1;
-  //   }
-
-  //   num_jian.onclick = function() {
-
-  //     if(input_num.value <= 0) {
-  //       input_num.value = 0;
-  //     } else {
-
-  //       input_num.value = parseInt(input_num.value) - 1;
-  //     }
-
-  //   }
-    
-    /* 如果在和后台做数据交互时，出现点击加减按钮的值无法传到后台的情况，可以用下面这种方式
-    $("body").on("click", ".num-jian", function(m) {
-      var obj = $(this).closest("ul").find(".input-num");
-      if(obj.val() <= 0) {
-           obj.val(0);
-      } else {
-           obj.val(parseInt(obj.val()) - 1);
-      }
-      obj.change();
-     });
-    
-    $("body").on("click", ".num-jia", function(m) {
-      var obj = $(this).closest("ul").find(".input-num");
-      obj.val(parseInt(obj.val()) + 1);
-      obj.change();
-    });*/
-
-
-
   
   // product detail 換圖
   $('.epr_pic').click (function () {
